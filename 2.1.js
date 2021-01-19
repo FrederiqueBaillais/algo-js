854ab4485f17fffb607827b2462af340b24715a7
Frederique Baillais <frederique.baillais@gmail.com>
2021-01-19 13:58:40 +0100

    new number 2.1.js

--- 2.1.js
+++ 2.1.js
@@ -1,6 +1,6 @@
 const readlineSync = require("readline-sync");
 
-let age = readlineSync.question('Please enter your age : ');
+let age = new Number(readlineSync.question('Please enter your age : '));
 
 if (age<18){
     console.log('You are not yet an adult');
