9f12867986b1171dd8356a45926267fdfba8a5c2
Frederique Baillais <frederique.baillais@gmail.com>
2021-01-19 15:37:10 +0100

    3.3.1.js

---
+++ 3.3.1.js
@@ -0,0 +1,9 @@
+let firsttab = [1,2,3,4,5];
+let copytab = new Array;
+
+for (let i = 0;i < firsttab.length; i++){
+    copytab.push(firsttab[i]);
+}
+
+console.log(firsttab);
+console.log(copytab);
