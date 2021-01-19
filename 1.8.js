af376692e84c1bb98c96c60e33fde97d5f051512
Frederique Baillais <frederique.baillais@gmail.com>
2021-01-19 10:42:21 +0100

    1.8.js changement

--- 1.8.js
+++ 1.8.js
@@ -1,8 +1,8 @@
 const readlineSync = require("readline-sync");
 
-let name = readlineSync.question("What's your firstname?");
-let place = readlineSync.question('Can you choose a place to be?');
-let time = readlineSync.question('Can you give me a time in minutes?');
-let object = readlineSync.question('Can you give me an object?');
+let name = readlineSync.question('Question 1/4 : What\'s your firstname?');
+let place = readlineSync.question('Question 2/4 : Can you choose a place to be?');
+let time = readlineSync.question('Question 3/4 : Can you give me a time in minutes?');
+let object = readlineSync.question('Question 4/4 Can you give me an object?');
 
 console.log('Once upon a time, three little pigs who met '+name+' in '+place+'. They chatted for '+time+' minutes and left '+name+' alone with '+object+' in his/her hand.');
