45523bfdc6f908b7ace9d4d56ba0d8e82c4ae879
Frederique Baillais <frederique.baillais@gmail.com>
2021-01-21 18:38:01 +0100

    6.3.js terminé

--- 6.3.js
+++ 6.3.js
@@ -1,14 +1,11 @@
-//reuse class Rectangle in 6.2.js
-//it should generate 1000 random instances of Rectangle
-//with random positions and sizes.
-//display all colliding rectangles amongst those that
-//were generated that way.
-let tableau = [];
+//reuse class Rectangle in 6.2.js. It should generate 1000 random instances of Rectangle with random
+//positions and sizes. Display all colliding rectangles amongst those that were generated that way.
 
 class Rectangle {
     //constructeur qui contient les attributs de la class et indique la position
     //initiale du rectangle avec topLeftXPos et topLeftYPos et indique sa largeur et sa longueur
-    constructor(topLeftXPos, topLeftYPos, width, length){
+    constructor(id, topLeftXPos, topLeftYPos, width, length){
+        this.id = id;
         this.topLeftXPos = topLeftXPos;
         this.topLeftYPos = topLeftYPos;
         this.width = width;
@@ -20,37 +17,40 @@ class Rectangle {
         var B = otherRectangle.topLeftXPos+otherRectangle.length;
         var C = this.topLeftYPos+this.width;
         var D = otherRectangle.topLeftYPos+otherRectangle.width;
-        let rectM;
-        for (let m = 0;m<tableau.length;m++){
-            rectM = tableau[m];
-            for (let p = 0;p<tableau.length;p++){
-                if ((((this.topLeftXPos < otherRectangle.topLeftXPos)&&(otherRectangle.topLeftXPos < A))
-                    || ((this.topLeftXPos < B)&&(B < A)))
-                    && (((this.topLeftYPos < otherRectangle.topLeftYPos)&&(otherRectangle.topLeftYPos < C))
-                    || ((this.topLeftYPos < D)&&(D < C)))) {
-                        return true;
-                    }
-            }
-            
+        //vérifie s'il y a une superposition de rectangles, si oui, renvoie true
+        if ((((this.topLeftXPos < otherRectangle.topLeftXPos)&&(otherRectangle.topLeftXPos < A))
+            || ((this.topLeftXPos < B)&&(B < A)))
+            && (((this.topLeftYPos < otherRectangle.topLeftYPos)&&(otherRectangle.topLeftYPos < C))
+            || ((this.topLeftYPos < D)&&(D < C)))) {
+                return true;
         }
-    }
-    //1000 rectangles
-    rect1000rand() {
-        for (let i = 0; i < 10; i++) {
-            
-            rect = new Rectangle(
-                Math.floor(Math.random() * (10 - 1) + 1),
-                Math.floor(Math.random() * (10 - 1) + 1),
-                Math.floor(Math.random() * (10 - 1) + 1),
-                Math.floor(Math.random() * (10 - 1) + 1)
-            );
-            tableau.push(rect);
-
+        else {
+            return false;
         }
-        console.log(rect.collides(rect));
-        console.log(tableau);
-        //return rect;
     }
 }
-let rect = new Rectangle();
-console.log(rect.rect1000rand());
+//crée un tableau contenant 1000 rectangles
+let tableau = [];
+for (let i = 1; i <= 1000; i++) {
+    rect = new Rectangle(
+        i,
+        Math.floor(Math.random() * 800) + 1,
+        Math.floor(Math.random() * 800) + 1,
+        Math.floor(Math.random() * 800) + 1,
+        Math.floor(Math.random() * 800) + 1
+    );
+    tableau.push(rect);
+}
+//vérifie s'il y a collision entre chaque rectangle et renvoie les rectangles qui se touchent
+//pour chaque rectangle, ...
+for (let m = 0; m < Math.round(tableau.length/2); m++) {
+    //... regarder les autres rectangles
+    for (let p = m+1; p < tableau.length; p++) {
+        if (tableau[m].collides(tableau[p])== true) {
+            //affiche les rectangles qui se tamponnent en renvoyant leur information sour forme de JSON
+            var myRect = JSON.stringify(tableau[m]);
+            var myRect2 = JSON.stringify(tableau[p]);
+            console.log(myRect+myRect2);
+        }
+    }
+}
